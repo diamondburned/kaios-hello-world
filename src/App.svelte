1d23d7708e0bad055fab65af1c06d6f5bd9767e2
diamondburned <diamond@arikawa-hi.me>
2023-03-23 16:58:56 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "@picocss/pico/css/pico.classless.min.css";
+
+  let message = "Hello, 世界!";
+</script>
+
+<main class="container">
+  <p>{message}</p>
+  <input bind:value={message} />
+</main>
+
+<style global lang="scss">
+</style>
